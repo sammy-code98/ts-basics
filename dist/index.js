7b74a0abbe5a915723586c9b41cd4bc21641e17c
sammy-code98 <sammysmart98@yahoo.com>
2022-09-04 23:31:52 +0100

    tuples

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,25 @@
 "use strict";
 // type of number
 let id = 5;
-console.log(id);
+console.log("ID is :", id);
+// type of string
+let userName = "sammy";
+console.log(userName);
+// type of boolean
+let isAuth = true;
+// type of any
+let x = 'helloe boy';
+// set x to another data type and no error shows
+x = true;
+// array of numbers
+let numArr = [1, 2, 3, 4, 5];
+console.log(numArr.push(3));
+let myArr = [1, 'true', true];
+// tuple  => with tuple you define the data types in the array
+let person = [2, 'samm', false];
+// tuple array
+let employeData;
+employeData = [
+    ['sam', 23],
+    ['nze', 22]
+];
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,42 @@
 // type of number
-let id : number = 5
+let id: number = 5;
 
-console.log(id);
+console.log("ID is :", id);
+
+// type of string
+
+let userName: string = "sammy";
+console.log(userName);
+
+// type of boolean
+
+let isAuth: boolean = true;
+
+// type of any
+
+let x: any = 'helloe boy'
+
+// set x to another data type and no error shows
+
+x = true
+
+// array of numbers
+
+let numArr : number[] = [1,2,3, 4,5]
+
+console.log(numArr.push(3));
+
+let myArr: any[] = [1, 'true' , true]
+
+
+// tuple  => with tuple you define the data types in the array
+
+let person : [number, string, boolean] = [2,'samm', false]
+
+// tuple array
+let employeData: [string, number][]
+
+employeData = [
+    ['sam', 23],
+    ['nze', 22]
+]
